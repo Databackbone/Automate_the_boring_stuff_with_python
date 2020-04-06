31c5cd0eaf4ce7c806a03d0c351698d8ab768eae
Robert <46407309+Databackbone@users.noreply.github.com>
2020-04-06 22:34:10 +0200

    rename file

--- Flow Chart.py
+++ Flow Chart.py
@@ -1,1 +1,14 @@
 print('Hello Flow Chart')
+#Boolean types
+
+erraten = False
+while not erraten:
+    my_age = int(input('wie alt bin ich? 30  '))
+    my_cat = input('tipe in Bernd  ')
+
+    if my_age > 18 and my_cat == 'Bernd':
+        print('Ganz genau meine Katze heiß Bernd')
+        erraten = True
+    else:
+        print('Das ist leider nicht richtig versuche es nochmal')
+
