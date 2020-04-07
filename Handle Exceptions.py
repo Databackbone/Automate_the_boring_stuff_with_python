d0bca7567eb6a341282d0cf0cb9e3607e9fd650d
Robert <46407309+Databackbone@users.noreply.github.com>
2020-04-07 09:57:03 +0200

    2 cases one try block internal of the the def one case try block wrapped around the function calls

--- Handle Exceptions.py
+++ Handle Exceptions.py
@@ -1,1 +1,31 @@
 #handle exceptions
+
+#For example if it could happen that you divide by zero
+from builtins import print
+
+
+def divide(number, by):
+    try:
+        return number/by
+    except ZeroDivisionError:
+        print('You cannot divide by zero')
+
+
+print(divide(6,0))
+
+#You can also have the try and except wraped around the function
+
+number = 42
+def divideby(bynumber):
+    return number/bynumber
+
+try:
+    print(divideby(3))
+    print(divideby(4))
+    print(divideby(0))
+    print(divideby(7))
+except ZeroDivisionError:
+    print('You cannot divide by zero wrapper')
+    #once you left a try block because of an exception there is no way back:(
+
+
